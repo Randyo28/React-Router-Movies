b27d7e6ca736988b83bc99e533c8145dc7cd7ae8
Randy Ortiz <Randyortiz28@gmail.com>
2021-02-22 19:03:23 -0500

    Rendered MovieList to Home Page

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import { useHistory, useParams} from 'react-router-dom'
 
 export default function MovieList(props) {
+
+  const {movies} = props
+  const {id} = useParams()
+  const history = useHistory()
+
+  const routeToMovie = () => {
+    history.push(`/movies/${id}`)
+    console.log(history)
+  }
   return (
-    <div className="movie-list">
+    <div onClick={routeToMovie} className="movie-list">
       {props.movies.map(movie => (
         <MovieDetails key={movie.id} movie={movie} />
       ))}
@@ -14,13 +24,15 @@ function MovieDetails(props) {
   const { title, director, metascore } = props.movie;
 
   return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
+    <div>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
+        </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
       </div>
     </div>
   );
